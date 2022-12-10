3c8a8c1ee59dd83bacff8052c36d85631d74d270
victorshinkevich <supostatekx@gmail.com>
2022-12-10 12:50:46 +0300

    manager, set and get spawns in separate functions

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,6 +1,6 @@
 use bytes::Bytes;
 use futures::future;
-use mini_redis::client;
+use mini_redis::client::{self, Client};
 use tokio::sync::{mpsc, oneshot};
 
 /// Multiple different commands are multiplexed over a single channel.
@@ -23,12 +23,53 @@ type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
 
 #[tokio::main]
 async fn main() {
-    let (tx, mut rx) = mpsc::channel(32);
+    const CHANNEL_BUFFER_SIZE: usize = 32;
+    const CLIENT_ADRESS: &str = "127.0.0.1:6378";
+    let (tx, rx) = mpsc::channel(CHANNEL_BUFFER_SIZE);
 
-    let read_futures: Vec<tokio::task::JoinHandle<()>> = (0..5)
+    let client = client::connect(CLIENT_ADRESS).await.unwrap();
+    let manager = build_manager(client, rx);
+
+    let number_of_futures = 5;
+    let read_futures = spawn_get_futures(number_of_futures, tx.clone());
+    let write_futures = spawn_set_futures(number_of_futures, tx);
+
+    future::join_all(read_futures).await;
+    future::join_all(write_futures).await;
+
+    manager.await.unwrap();
+}
+
+fn build_manager(
+    mut client: Client,
+    mut rx: tokio::sync::mpsc::Receiver<Command>,
+) -> tokio::task::JoinHandle<()> {
+    tokio::spawn(async move {
+        while let Some(cmd) = rx.recv().await {
+            match cmd {
+                Command::Get { key, resp } => {
+                    let res = client.get(&key).await;
+                    // Ignore errors
+                    let _ = resp.send(res);
+                }
+                Command::Set { key, val, resp } => {
+                    let res = client.set(&key, val).await;
+                    // Ignore errors
+                    let _ = resp.send(res);
+                }
+            }
+        }
+    })
+}
+
+fn spawn_get_futures(
+    number_of_futures: usize,
+    transmitter: tokio::sync::mpsc::Sender<Command>,
+) -> Vec<tokio::task::JoinHandle<()>> {
+    let read_futures: Vec<tokio::task::JoinHandle<()>> = (0..number_of_futures)
         .into_iter()
         .map(|thread_number| {
-            let transmitter = tx.clone();
+            let transmitter = transmitter.clone();
             tokio::spawn(async move {
                 let (resp_tx, resp_rx) = oneshot::channel();
                 let key = format!("foo#{}", thread_number).into();
@@ -45,11 +86,17 @@ async fn main() {
             })
         })
         .collect();
+    read_futures
+}
 
-    let write_futures: Vec<tokio::task::JoinHandle<()>> = (0..5)
+fn spawn_set_futures(
+    number_of_futures: usize,
+    transmitter: tokio::sync::mpsc::Sender<Command>,
+) -> Vec<tokio::task::JoinHandle<()>> {
+    let write_futures: Vec<tokio::task::JoinHandle<()>> = (0..number_of_futures)
         .into_iter()
         .map(|thread_number| {
-            let transmitter = tx.clone();
+            let transmitter = transmitter.clone();
             tokio::spawn(async move {
                 let (resp_tx, resp_rx) = oneshot::channel();
                 let key = format!("foo#{}", thread_number).into();
@@ -70,30 +117,5 @@ async fn main() {
             })
         })
         .collect();
-
-    drop(tx);
-
-    let manager = tokio::spawn(async move {
-        let mut client = client::connect("127.0.0.1:6378").await.unwrap();
-
-        while let Some(cmd) = rx.recv().await {
-            match cmd {
-                Command::Get { key, resp } => {
-                    let res = client.get(&key).await;
-                    // Ignore errors
-                    let _ = resp.send(res);
-                }
-                Command::Set { key, val, resp } => {
-                    let res = client.set(&key, val).await;
-                    // Ignore errors
-                    let _ = resp.send(res);
-                }
-            }
-        }
-    });
-
-    future::join_all(read_futures).await;
-    future::join_all(write_futures).await;
-
-    manager.await.unwrap();
+    write_futures
 }
